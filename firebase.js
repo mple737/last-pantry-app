b1d862aea200d9b06665062e78f81a3c3b62e3f5
ArsenalBAck2004 <utphuongmap@gmail.com>
2024-08-04 23:34:18 -0400

    Fixed change

--- firebase.js
+++ firebase.js
@@ -9,13 +9,14 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  authDomain: "pantry-firstprj.firebaseapp.com",
-  projectId: "pantry-firstprj",
-  storageBucket: "pantry-firstprj.appspot.com",
-  messagingSenderId: "222088674076",
-  appId: "1:222088674076:web:65602148b4db80c2b4d8d9",
-  measurementId: "G-KS2Z9HW2L8"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
